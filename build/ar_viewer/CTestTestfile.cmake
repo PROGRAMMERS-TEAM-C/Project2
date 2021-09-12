5264c011a89bff7c58632807a00a6ef9ef6e2693
choyoungjin0928 <agapao12345@naver.com>
2021-09-12 20:15:05 +0900

    초본 업로드

--- build/ar_viewer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/selfdriving/catkin_ws/src/ar_viewer
-# Build directory: /home/selfdriving/catkin_ws/build/ar_viewer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
