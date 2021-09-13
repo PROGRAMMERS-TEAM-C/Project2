7137bbd5c19b048b1efcb0310361aace62d1249a
LimKyeongmuk <koungmuk1975@gmail.com>
2021-09-13 21:45:29 +0900

    src 파일 구조화

--- src/pid.py
+++ src/pid.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
+#!/user/bin/env python
 # -*- coding: utf-8 -*-
 
 class PID():
-	def __init__(self,kp,ki,kd):
-		self.kp = kp
-		self.ki = ki
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
         self.kd = kd
-		self.p_error = 0.0
-		self.i_error = 0.0
-		self.d_error = 0.0
-		
-	#P term = Kp x (Error), I term = Ki x (누적 Error), D term = Kd x (Error 변화량)
-	def pid_control(self,cte):
-		self.d_error = cte - self.p_error #cte = 화면의 중앙점과 좌우차선의 중점과의 차이, D값은 현재 cte 값과 이전 p값의 차이 값을 적용
-		self.p_error = cte #P값은 CTE값을 그대로 적용
-		self.i_error += cte #I값은 CTE값을 계속 더해서 누적한 값을 적용
-		
-		return self.kp*self.p_error + self.ki*self.i_error + self.kd*self.d_error
+        self.p_error = 0.0
+        self.i_error = 0.0
+        self.d_error = 0.0
+
+    def pid_control(self, cte):
+        self.d_error = cte - self.p_error	# (현재 시간 error) - (이전 시간 error)
+        self.p_error = cte
+        if abs(self.i_error + cte) < 2000:	# 시간이 지날수록 계속 누적되면 i_error가 p나 d에 비해 너무 커지기에 상한선 설정
+            self.i_error += cte			# ki = 0.0005인 경우, 0.0005 x 2000 = 1. 적분 부분이 1도 이상 영향 주지 않게끔 설정
+
+        return self.kp*self.p_error + self.ki*self.i_error + self.kd*self.d_error
